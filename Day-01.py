e8e36b8e283f4423ad66d2c34d126a3323eb79d4
Rajat <rajatsb77@gmail.com>
2026-08-11 20:32:53 +0530

    Day 001: Variables and basic operations

---
+++ Day-01.py
@@ -0,0 +1,18 @@
+'''
+----- Student Profile -----
+'''
+Name = "Rajat"
+Age  = 23
+Maths,Stats = 85, 78
+Total = Maths + Stats
+Average = Total / 2
+Python_Per_Day = 2
+Python_Per_Week = Python_Per_Day * 7
+print("Name:", Name)
+print("Age:", Age)
+print("Maths:", Maths)
+print("Stats:", Stats)
+print("Total:", Total)
+print("Average:", Average)
+print("Python_Per_Day:", Python_Per_Day)
+print("Python_Per_Week:", Python_Per_Week)
